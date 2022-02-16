7e6f3f383648a01bc9c1b2bb4276064dbae2bb5a
mkanenobu <mitsuru.kanenobu@gmail.com>
2022-02-16 14:01:12 +0900

    fix build script

--- scripts/build.js
+++ scripts/build.js
@@ -1,23 +1,24 @@
 const build = require("esbuild").build;
 
-const env = process.env.NODE_ENV || "development";
+const availableEnvValues = ["development", "production", "test"];
+const env = availableEnvValues.includes(process.env.NODE_ENV)
+  ? process.env.NODE_ENV
+  : "development";
 
 /**
  * @type {{development: import("esbuild").BuildOptions, production: import("esbuild").BuildOptions}}
  */
 const paramsByEnv = {
   development: {
-    minify: false,
     watch: {
       onRebuild: (error, result) => {
-        console.log("Rebuild: ");
-        console.log({ error, result });
+        console.log("Rebuild: ", { error, result });
       },
     },
+    sourcemap: true,
     outfile: "./dist/dev-index.js",
   },
   production: {
-    minify: true,
     outfile: "./dist/index.js",
   },
 };
@@ -25,7 +26,10 @@ const paramsByEnv = {
 build({
   entryPoints: ["./src/index.ts"],
   bundle: true,
-  target: "es2020",
+  minify: false,
+  platform: "node",
+  external: ["./node_modules/*"],
+  target: "node10",
   ...paramsByEnv[env],
 })
   .then((buildResult) => {
